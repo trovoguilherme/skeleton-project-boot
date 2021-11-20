ade38efb0e6eca20a09bc882f37ffb32bf713d71
Guilherme <guinavy2011@live.com>
2021-11-20 00:59:18 -0300

    Criação do deletar por id

--- src/main/java/br/com/guilherme/repository/ServicoRepository.java
+++ src/main/java/br/com/guilherme/repository/ServicoRepository.java
@@ -2,7 +2,9 @@ package br.com.guilherme.repository;
 
 import br.com.guilherme.model.ServicoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -12,5 +14,10 @@ public interface ServicoRepository extends JpaRepository<ServicoModel, Long> {
 
     @Query("SELECT s from ServicoModel s WHERE s.categoria = ?1")
     List<ServicoModel> findServicoByCategoria(String categoria);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE ServicoModel WHERE id = ?1")
+    void deleteById(long id);
 
 }
