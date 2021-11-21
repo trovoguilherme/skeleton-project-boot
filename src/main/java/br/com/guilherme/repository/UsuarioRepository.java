dfa69ece5227b93986c2bc537a54b5600d1816f9
Guilherme <guinavy2011@live.com>
2021-11-21 02:30:58 -0300

    Removido classe e interface removida

--- src/main/java/br/com/guilherme/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.guilherme.repository;
-
-import br.com.guilherme.model.UsuarioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, String> {
-
-}
