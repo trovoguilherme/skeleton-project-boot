7d048e14c6e1ecb627c96224769a65a5a3937ef6
Guilherme <guinavy2011@live.com>
2021-11-21 02:38:01 -0300

    Criado a controller de Login, cadastro e listagem de usuários

---
+++ src/main/java/br/com/guilherme/controller/AppController.java
@@ -0,0 +1,55 @@
+package br.com.guilherme.controller;
+
+import br.com.guilherme.model.UserModel;
+import br.com.guilherme.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/entrar")
+public class AppController {
+
+	private static final String LOGIN_FOLDER = "login/";
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@GetMapping()
+	public String viewHomePage() {
+		return LOGIN_FOLDER + "index";
+	}
+	
+	@GetMapping("/register")
+	public String showRegistrationForm(@ModelAttribute("userModel") UserModel userModel, Model model) {
+		model.addAttribute("user", new UserModel());
+		
+		return LOGIN_FOLDER + "signup-form";
+	}
+	
+	@PostMapping("/process-register")
+	public String processRegister(UserModel userModel) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(userModel.getPassword());
+		userModel.setPassword(encodedPassword);
+		
+		userRepo.save(userModel);
+		
+		return LOGIN_FOLDER + "register-success";
+	}
+	
+	@GetMapping("/users")
+	public String listUsers(Model model) {
+		List<UserModel> listUserModels = userRepo.findAll();
+		model.addAttribute("listUsers", listUserModels);
+		
+		return LOGIN_FOLDER + "users";
+	}
+}
