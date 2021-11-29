8ec0a90cc6738016a41a4576ef59b466ed8e1742
Guilherme <guinavy2011@live.com>
2021-11-29 01:32:11 -0300

    Criado a classe de Comentário

---
+++ src/main/java/br/com/guilherme/model/ComentarioModel.java
@@ -0,0 +1,34 @@
+package br.com.guilherme.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "TB_COMENTARIO")
+public class ComentarioModel {
+
+    @Id
+    @Column(name = "ID_COMENTARIO")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idComentario;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "MENSAGEM")
+    private String mensagem;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_USUARIO")
+    private UserModel usuario;
+
+}
