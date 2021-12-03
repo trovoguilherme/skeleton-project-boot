65641156ba91643c5598acefbd7453223bce2932
Guilherme <guinavy2011@live.com>
2021-12-03 00:06:52 -0300

    Adicionado o findUsersWithoutMe

--- src/main/java/br/com/guilherme/repository/UserRepository.java
+++ src/main/java/br/com/guilherme/repository/UserRepository.java
@@ -4,8 +4,13 @@ import br.com.guilherme.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 	@Query("SELECT u FROM UserModel u WHERE u.email = ?1")
 	public UserModel findByEmail(String email);
+
+	@Query("SELECT u FROM UserModel u WHERE u.email != ?1")
+	public List<UserModel> findUsersWithoutMe(String email);
 	
 }
