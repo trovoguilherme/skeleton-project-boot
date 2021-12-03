f28018a1e6b54af1e07aa91cbc4682a421f53006
Guilherme <guinavy2011@live.com>
2021-12-03 03:00:45 -0300

    Criado a controller do histórico

---
+++ src/main/java/br/com/guilherme/controller/HistoricoController.java
@@ -0,0 +1,30 @@
+package br.com.guilherme.controller;
+
+import br.com.guilherme.model.UserModel;
+import br.com.guilherme.repository.ServicoRepository;
+import br.com.guilherme.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/historico")
+public class HistoricoController {
+
+    @Autowired
+    ServicoRepository servicoRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping()
+    public String open(Model model, Authentication authentication) {
+        UserModel user = userRepository.findByEmail(authentication.getName());
+        model.addAttribute("servicos", servicoRepository.findAll());
+        return "home-contratante/historico/historico";
+    }
+
+}
