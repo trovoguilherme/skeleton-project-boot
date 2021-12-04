7eb7563c9399a933fe8578a5b1e6507506ed6910
Guilherme <guinavy2011@live.com>
2021-12-04 19:13:14 -0300

    Enpoint para abrir um status

--- src/main/java/br/com/guilherme/controller/HistoricoController.java
+++ src/main/java/br/com/guilherme/controller/HistoricoController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,6 +26,22 @@ public class HistoricoController {
         UserModel user = userRepository.findByEmail(authentication.getName());
         model.addAttribute("servicos", servicoRepository.findMyServices(user));
         return "home-contratante/historico/historico";
+    }
+
+    @GetMapping("/{status}")
+    public String openStatus(@PathVariable("status") String status, Model model) {
+        String page = "";
+        if (status.equalsIgnoreCase("aberto")) {
+            page = "home-contratante/historico/aberto";
+        } else if(status.equalsIgnoreCase("fazendo")) {
+            page = "home-contratante/historico/fazendo";
+        } else {
+            page = "home-contratante/historico/finalizado";
+        }
+
+        model.addAttribute("servicosPorStatus", servicoRepository.findServicosByStatus(status));
+
+        return page;
     }
 
 }
