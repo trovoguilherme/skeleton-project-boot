159751c26a7aceb702ab097cd05b6be07034f5f3
Guilherme <guinavy2011@live.com>
2021-12-06 01:31:36 -0300

    Ainda não faz nada

--- src/main/resources/sql/populate-database.sql
+++ src/main/resources/sql/populate-database.sql
@@ -1,11 +1,3 @@
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO,CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Back-end', 'Nullam placerat tempor dui. Curabitur ut nulla mollis, luctus orci a, maximus sapien. Fusce blandit.', 'java');
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO, CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Front-end', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum odio vitae magna fermentum.', 'html');
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO, CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Youtube', 'Donec venenatis elementum enim. Vestibulum sit amet facilisis nunc. Pellentesque pulvinar, nisi sit.', 'videos');
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO, CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Twitch', 'Morbi feugiat lectus sed enim tempor, sit amet venenatis sapien euismod. Vivamus turpis sapien duis. ', 'videos');
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO, CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Web', 'Aliquam eget tortor fermentum, consectetur libero ut, auctor elit. Vestibulum sollicitudin, est leo.', 'html');
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO, CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Back-end Java', 'Etiam dignissim tristique urna a consequat. Nam neque leo, tristique ut cursus a, semper eget proin.', 'java');
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO, CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Tik Tok', 'Maecenas fringilla scelerisque metus, et varius lectus egestas eget. Curabitur tristique magna quis.', 'videos');
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO, CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Front end', 'Nunc at eleifend lacus. Curabitur eu tortor eu nisl vulputate porttitor. Donec semper, nisi sit non.', 'html');
-INSERT INTO TB_SERVICO (ID, TITULO, DESCRICAO, CATEGORIA) VALUES (SERVICO_SEQ.NEXTVAL, 'Canal boom', 'Duis pellentesque arcu enim, eget ultrices odio placerat in. Mauris facilisis sem in euismod ligula.', 'videos');
-
-INSERT INTO USERS (ID, EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, usuario, usuarioServico, CPF, ESTADO, TELEFONE, biografia, TIPO_DA_CONTA) VALUES (1, );
+INSERT INTO USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, CPF, ESTADO, TELEFONE, biografia, TIPO_DA_CONTA) VALUES ('gui@live.com', '12345', 'Guilherme', 'Trovo', '11111111111', 'SP', '11955801920', 'Descrição aquiiiii kkk', 'prestador');
+INSERT INTO USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, CPF, ESTADO, TELEFONE, biografia, TIPO_DA_CONTA) VALUES ('tangobalango@test1.com', '12345', 'Tango', 'Balango', '11111111111', 'SP', '11955801920', 'Descrição aquiiiii kkk', 'prestador');
+INSERT INTO USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, CPF, ESTADO, TELEFONE, biografia, TIPO_DA_CONTA) VALUES ('chimi@test2.com', '12345', 'Chimi', 'Changas', '11111111111', 'SP', '11955801920', 'Descrição aquiiiii kkk', 'prestador');
