072cbd887a4ea9cc637b0efbf29ff4cef12c9017
Guilherme <guinavy2011@live.com>
2021-12-09 22:40:38 -0300

    Adicionado lógica para pagar o serviço

--- src/main/java/br/com/guilherme/controller/CheckoutController.java
+++ src/main/java/br/com/guilherme/controller/CheckoutController.java
@@ -1,16 +1,27 @@
 package br.com.guilherme.controller;
 
+import br.com.guilherme.model.ServicoModel;
+import br.com.guilherme.repository.ServicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/checkout")
 public class CheckoutController {
 
+    @Autowired
+    ServicoRepository servicoRepository;
 
-    @GetMapping()
-    public String open() {
+    @GetMapping("/{id}")
+    public String open(@PathVariable("id") long id, Model model) {
+        ServicoModel findServico = servicoRepository.findById(id).get();
+        findServico.setStatus("finalizado");
+        servicoRepository.save(findServico);
+        model.addAttribute("servico", servicoRepository.findById(id).get());
         return "checkout/checkout";
     }
 
